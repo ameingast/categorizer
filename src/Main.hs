2c26b44c3517c42b0fda5050c0f8f969219d3175
Andreas Meingast <ameingast@gmail.com>
2013-03-13 08:57:55 +0100

    Fixed static middleware policy

--- src/Main.hs
+++ src/Main.hs
@@ -4,14 +4,15 @@ import Categorizer.Wai.Application(application)
 import Categorizer.Configuration(Configuration(..), loadConfiguration)
 import Categorizer.Text.Matcher(loadMatcher)
 import Network.Wai.Handler.Warp(run)
-import Network.Wai.Middleware.Static(staticPolicy, addBase)
+import Network.Wai.Middleware.Static(only, addBase, (<|>), Policy, staticPolicy)
 
 main :: IO ()
 main = do
     c <- loadConfiguration
     m <- loadMatcher 
 
-    let static = staticPolicy (addBase "static")
-    let app = application m
-
-    run (_port c) (static app)
+    run (_port c) (staticPolicy policy (application m))
+    where
+        policy :: Policy
+        policy = only [("", "static/index.html"), ("/", "static/index.html")] 
+                <|> addBase "static"
